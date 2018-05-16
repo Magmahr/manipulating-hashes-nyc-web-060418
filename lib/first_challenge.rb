a45f6789fcca444f9f2d4d58c5ccb5e7fbc5f133
Matt Maher <mattmaher0@gmail.com>
2018-05-16 15:21:02 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,11 @@ def first_challenge
   }
 
   #your code here
-  contacts["Freddy Mercury"][favorite_icecream_flavors].each do |flavors| flavors.delete_if flavors == "strawberry"
+  contacts["Freddy Mercury"].each do |cat|
+    if cat == favorite_icecream_flavors
+      favorite_icecream_flavors.each do |flavors|   
+        |flavors| flavors.delete_if flavors == "strawberry"
+      end  
   end
   
   #remember to return your newly altered contacts hash!
